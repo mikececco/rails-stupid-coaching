09d1428fa92ab52f247c31c3809f46d3810eace8
Mike Cecconello <mikececco2000@gmail.com>
2023-02-20 12:44:13 +0100

    Added backlink

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,6 +2,10 @@
 
 class AnswersController < ApplicationController
   def answer
-    @answer = params["answer"]
+    @answer = if params['answer'].start_with?('I am') then 'Great'
+              elsif params['answer'].end_with?('?') then 'Silly question, get dressed and go to work!.'
+              else
+                "I don't care, get dressed and go to work!    "
+              end
   end
 end
